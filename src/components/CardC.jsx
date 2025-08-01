be5c776bab59516f252c0c4c775dd3decac1301f
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-01 20:22:53 +0200

    creato componente card

---
+++ src/components/CardC.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { Link } from "react-router";
+export default function CardC(props) {
+  const [isVisible, setIsVisible] = useState(false);
+
+  return (
+    <>
+      {props.data.map((movie) => {
+        return (
+          <div className="col" key={movie.id}>
+            <div className="card h-100 text-bg-dark">
+              <img
+                src={` http://localhost:3004/${movie.image}`}
+                className="card-img"
+                alt="Image Film"
+              />
+              <div
+                className="card-img-overlay  p-0"
+                onMouseEnter={() => setIsVisible(movie.id)}
+                onMouseLeave={() => setIsVisible(false)}
+              >
+                <div
+                  className={`hidden ${isVisible === movie.id ? "show" : ""}`}
+                >
+                  <h5 class="card-title text-center py-3 fw-bold">
+                    {" "}
+                    {movie.title}
+                  </h5>
+                  <div className="d-flex justify-content-center align-items-center">
+                    <Link
+                      className="btn btn-outline-danger border-3 fw-bold mx-2"
+                      to={`/${movie.id}`}
+                    >
+                      Vai al film
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        );
+      })}
+    </>
+  );
+}
--- src/components/MainHome.jsx
+++ src/components/MainHome.jsx
@@ -1,9 +1,8 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
-
+import CardC from "./CardC";
 export default function MainHome() {
   const [dataMovies, setDataMovies] = useState([]);
-  const [isVisible, setIsVisible] = useState(false);
   const url = "http://localhost:3004/movies";
   function gnrData() {
     fetch(url)
@@ -14,49 +13,12 @@ export default function MainHome() {
       });
   }
   useEffect(gnrData, []);
-
   return (
     <>
       <main>
         <div className="container">
-          <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2   row-cols-lg-3  g-3">
-            {dataMovies.map((movie) => {
-              return (
-                <div className="col" key={movie.id}>
-                  <div className="card h-100 text-bg-dark">
-                    <img
-                      src={` http://localhost:3004/${movie.image}`}
-                      className="card-img"
-                      alt="Image Film"
-                    />
-                    <div
-                      className="card-img-overlay  p-0"
-                      onMouseEnter={() => setIsVisible(movie.id)}
-                      onMouseLeave={() => setIsVisible(false)}
-                    >
-                      <div
-                        className={`hidden ${
-                          isVisible === movie.id ? "show" : ""
-                        }`}
-                      >
-                        <h5 class="card-title text-center py-3 fw-bold">
-                          {" "}
-                          {movie.title}
-                        </h5>
-                        <div className="d-flex justify-content-center align-items-center">
-                          <Link
-                            className="btn btn-outline-danger   mx-2"
-                            to={`/${movie.id}`}
-                          >
-                            Vai al film
-                          </Link>
-                        </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              );
-            })}
+          <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2  row-cols-lg-4  g-3">
+            <CardC data={dataMovies}></CardC>
           </div>
         </div>
       </main>
