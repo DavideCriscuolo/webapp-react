212cd7c1f098d54db19c9b5731953a7ab8f64098
DavideCriscuolo <davide.alcatel@gmail.com>
2025-08-01 16:08:15 +0200

    creata funzione per prendere le recensioni del film dall id passato tramite il params

--- src/pages/SingleMovie.jsx
+++ src/pages/SingleMovie.jsx
@@ -1,7 +1,29 @@
+import { use, useState, useEffect } from "react";
+import { useParams } from "react-router";
+
 export default function SingleMovie() {
+  const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+  const url = `http://localhost:3004/movies/movie/${id}`;
+
+  function gnrDataMovvie() {
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => {
+        setMovie(data);
+      });
+  }
+  useEffect(gnrDataMovvie, []);
+  console.log(movie);
   return (
     <>
-      <h1>SingleMovie</h1>
+      <div>
+        <div>
+          {movie.map((item) => {
+            return <p>{item.text}</p>;
+          })}
+        </div>
+      </div>
     </>
   );
 }
